ffe5c1193b1f6a16144d136a421d157e9412dba6
Daniel Felix <felixdaniel9@gmail.com>
2023-06-02 09:21:02 -0300

    Creating login page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+import './style.css';
+
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <>
-      <h1>Página Login</h1>
-    </>
+    <div className='container-center'>
+      <div className='login'>
+        <div className='login-area'>
+          <img src={logo} alt="System logo" />
+        </div>
+
+        <form>
+          <h1>Enter</h1>
+
+          <input
+            type='text'
+            placeholder='email@email.com'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type='password'
+            placeholder='********'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <buttom type="submit">Access</buttom>
+        </form>
+
+        <Link to="/register">Create an account</Link>
+      </div>
+    </div>
   )
 }
 
